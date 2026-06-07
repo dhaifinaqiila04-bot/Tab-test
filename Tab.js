bed463c5f504e9c77095a8b7c3e8a900ace2a034
DhaifinAqiila <dhaifinaqiila04@gmail.com>
2026-06-07 18:08:38 +0700

    Update Tab.js

--- Tab.js
+++ Tab.js
@@ -1,20 +1,46 @@
 function openCity(evt, cityName) {
-  // Declare all variables
   var i, tabcontent, tablinks;
 
-  // Get all elements with class="tabcontent" and hide them
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
 
-  // Get all elements with class="tablinks" and remove the class "active"
   tablinks = document.getElementsByClassName("tablinks");
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
 
-  // Show the current tab, and add an "active" class to the button that opened the tab
   document.getElementById(cityName).style.display = "block";
   evt.currentTarget.className += " active";
 }
+
+
+function Cookie() {
+  const cookieBox = document.querySelector(".cookie");
+  const cancelButton = document.querySelector(".btn-cancel");
+  const acceptButton = document.querySelector(".btn-accept");
+
+  setTimeout(() => {
+    if (cookieBox) {
+      cookieBox.style.display = 'block';
+    }
+  }, 3000);
+
+  if (cancelButton && cookieBox) {
+    cancelButton.addEventListener('click', () => {
+      cookieBox.style.display = 'none';
+    });
+  }
+
+  if (acceptButton && cookieBox) {
+    acceptButton.addEventListener('click', () => {
+      cookieBox.style.display = 'none';
+    });
+  }
+}
+
+Cookie();
+
+
+
